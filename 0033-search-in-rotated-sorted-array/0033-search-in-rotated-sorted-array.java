b74fed6c9c6ca484aa4a48df521e77a10fbc6775
Jay Jaiswal <jayjaiswal467@gmail.com>
2026-08-24 14:29:41 +0530

    Time: 0 ms (100%), Space: 43.6 MB (82.79%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int search(int[] a, int target) {
+        
+        int low=0;
+        int n = a.length;
+        int high = a.length-1;
+
+        while(low<=high){
+            int guess = (low+high)/2;
+
+            if(a[guess]==target){
+                return guess;
+            }
+
+            //PART 1 MEIN GUESS LAND HUA HAI
+            if(a[guess]>a[n-1]){
+                if(a[guess]<target){
+                    //move right
+                    low=guess+1;
+                }
+                else{
+                    if(a[0]>target){
+                        //move right
+                        low=guess+1;
+                    }
+                    else{
+                        high=guess-1;
+                    }
+                }
+            continue;
+            }
+            
+
+            //PART 2 MEIN GUESS LAND HUA HAI
+            if(a[guess]>target){
+                //move left
+                high=guess-1;
+            }
+            else{
+                if(a[n-1]<target){
+                    high=guess-1;
+                }
+                else{
+                    low=guess+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
